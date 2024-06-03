8e722a26e788496462f451d40292254cd26d217d
Coty Woodside <cotyw@tlgcohort.com>
2024-06-03 16:59:05 -0500

    end of day

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,8 +8,7 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PoemClient {
 
@@ -18,7 +17,7 @@ public class PoemClient {
      */
     public static void main(String[] args) {
         // readPoem();
-        // writePoem();
+         writePoem();
     }
 
     /**
@@ -34,9 +33,11 @@ public class PoemClient {
      * The try-with-resources below allows you to initialize the stream and auto-close it.
      */
     private static void readPoem() {
-        // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+        String line;
+        while((line = reader.readLine()) != null) {
+            System.out.println(line);
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -55,6 +56,14 @@ public class PoemClient {
      * Use a try-with-resources to initialize the stream and auto-close it.
      */
     private static void writePoem() {
-        // TODO
+      try (PrintWriter writer = new PrintWriter(new FileWriter("haiku.txt"))) {
+        writer.println("i'm not spending 10 minutes to write haiku - this is a java class'");
+      }
+      catch (IOException e){
+          e.printStackTrace();
+      }
     }
 }
+//      this is my haiku
+//        it doesnt matter if it
+//        really is haiku
