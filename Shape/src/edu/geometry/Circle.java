84018b440d45273900489c6c638c0c4a8e72b67b
Coty Woodside <cotyw@tlgcohort.com>
2024-06-03 09:58:31 -0500

    reformat code in shape module

--- Shape/src/edu/geometry/Circle.java
+++ Shape/src/edu/geometry/Circle.java
@@ -3,13 +3,16 @@ package edu.geometry;
 
 public class Circle implements Shape {
     private final double radius;
+
     public Circle(double radius) {
         this.radius = radius;
     }
+
     @Override
     public double getArea() {
         return Math.PI * (radius * radius);
     }
+
     public double getRadius() {
         return radius;
     }
